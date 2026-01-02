4dcf0f0c3c490a9c1d71002861a5b0efd6238582
itsfredrick <nyamsifredrick@gmail.com>
2026-01-02 22:19:32 +0100

    fix(marketing): force webpack build to resolve turbopack conflict

--- apps/marketing/next.config.js
+++ apps/marketing/next.config.js
@@ -16,7 +16,6 @@ const nextConfig = {
             static: 180,
         },
     },
-    turbo: {}, // Opt-in to Turbopack
     images: {
         formats: ["image/avif", "image/webp"],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
